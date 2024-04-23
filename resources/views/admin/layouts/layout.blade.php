eadc3e3f42873842382a19491f79b71334b0bdad
VladimirR190 <0vovaromanov0@mail.ru>
2024-04-23 16:59:23 +0500

    Изменения по мелочи

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -332,7 +332,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="/admin/tags/index" class="nav-link">
+                                    <a href="/admin/tags" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Список тегов</p>
                                     </a>
